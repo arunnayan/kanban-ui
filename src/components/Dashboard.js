62892a688568a80303a30c4d8363c59116841613
Arun Singh <arunnayan.28@gmail.com>
2019-08-17 01:26:22 -0700

    Added submit form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
-
+import CreateProjectButton from "./Project/CreateProjectButton";
 export default class Dashboard extends Component {
   render() {
     return (
@@ -10,14 +10,7 @@ export default class Dashboard extends Component {
             <div className="col-md-12">
               <h1 className="display-4">Projects</h1>
               <br />
-              <a
-                href="ProjectForm.html"
-                className="btn btn-lg btn-info"
-                role="button"
-                aria-pressed="true"
-              >
-                Create a Project
-              </a>
+              <CreateProjectButton />
               <br />
               <hr />
               <ProjectItem />
